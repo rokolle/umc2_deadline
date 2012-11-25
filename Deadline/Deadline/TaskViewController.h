dadb119e9f8718b33d614df80713ab8992901869
Benedict Endemann <B.Endemann@gmx.de>
2012-11-25 11:37:36 +0100

    Task View hinzugefuegt. Detail View bearbeitet Wir werden uns ständig über XCode aufregen, bis wir kotzen!!!

---
+++ Deadline/Deadline/TaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  TaskViewController.h
+//  Deadline
+//
+//  Created by Benedict Endemann on 25.11.12.
+//  Copyright (c) 2012 SeRoBe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TaskViewController : NSObject
+
+@end
---
+++ Deadline/Deadline/TaskViewController.m
@@ -0,0 +1,13 @@
+//
+//  TaskViewController.m
+//  Deadline
+//
+//  Created by Benedict Endemann on 25.11.12.
+//  Copyright (c) 2012 SeRoBe. All rights reserved.
+//
+
+#import "TaskViewController.h"
+
+@implementation TaskViewController
+
+@end
